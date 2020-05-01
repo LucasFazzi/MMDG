ae07c581844f070b0262ebf5c7bdec9f061c8cc6
Lucas Z Fazzi <lzfazzi@gmail.com>
2020-05-01 20:46:22 -0300

    Add files via upload

--- Movement_Test/KinematicBody2D.gd
+++ Movement_Test/KinematicBody2D.gd
@@ -1,6 +1,5 @@
 extends KinematicBody2D
 
-
 export var MOVE_SPEED = 300
 export var JUMP_FORCE = 1100
 export var GRAVITY = 50
@@ -8,7 +7,7 @@ export var GRAVITY_WALL = 10
 export var MAX_FALL_SPEED = 1000
 export var Y_VELO = 0
 export var JUMP = true
-
+export var DOUBLE_JUMP = false
 
 func _ready():
 	add_group()
@@ -40,15 +39,15 @@ func move():
 			jump()
 			return
 		if is_on_wall():
+#tentativa de fazer um grab wall meio vagabundo
 			grab()
+#corte do pulo pra desboostar
 			while Input.is_action_just_pressed("jump"):
 				jump()
 				return
 
 	elif Input.is_action_just_released("jump"):
 		jump_cut()
-#tentativa de fazer um grab wall meio vagabundo
-
 	while is_on_ceiling():
 		Y_VELO += GRAVITY
 		return
@@ -56,16 +55,25 @@ func move():
 		Y_VELO = MAX_FALL_SPEED
 		return
 	while is_on_floor():
+		DOUBLE_JUMP = true
 		JUMP = true
 		return
+	if not is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			double_jump()
 
 func grab():
-		Y_VELO = GRAVITY_WALL
-		JUMP = true
-
-func jump():
-	Y_VELO = - JUMP_FORCE
+	Y_VELO = GRAVITY_WALL
 	JUMP = true
+	DOUBLE_JUMP = true
+func jump():
+	Y_VELO =- JUMP_FORCE
+	JUMP = true
+#pulo duplo
+func double_jump():
+	if DOUBLE_JUMP == true:
+		Y_VELO =- JUMP_FORCE
+		DOUBLE_JUMP = false
 func jump_cut():
 	while Y_VELO < -100:
 		Y_VELO = -70
